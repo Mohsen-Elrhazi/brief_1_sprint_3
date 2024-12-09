3be338459c88f8ad1bddd0de387c6848b6cf4e1e
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2024-12-09 13:00:49 +0100

    Créer la Table review

--- films_db.sql
+++ films_db.sql
@@ -1,7 +1,17 @@
 CREATE DATABASE films_db;
 
+USE films_db;
 CREATE TABLE subscription(
 SubscriptionID INT PRIMARY KEY AUTO_INCREMENT,
 SubscriptionType VARCHAR(50) CHECK (SubscriptionType IN ('Basic','Standard','Premium')),
 MonthlyFee DECIMAL(10,2)
+);
+
+CREATE TABLE review(
+    ReviewID INT PRIMARY KEY AUTO_INCREMENT,
+    UserID INT,
+    MovieID INT,
+    Rating INT,
+    ReviewText TEXT,
+    ReviewDate DATE
 );
