ea657e0e22d76d5ccac67aaf867c7f714ce39c86
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2024-12-09 14:11:15 +0100

    Créeation de la table watchhistory

--- films_db.sql
+++ films_db.sql
@@ -1,12 +1,21 @@
+
+ --name: Mohsen Elrhazi
+ --gmail: mohsenelrhazi3@gmail.com
+
+ --Créeaton de la base de données films_db
 CREATE DATABASE films_db;
 
+-- Utilisation de la base de données
 USE films_db;
+
+--Créeation de la table subscription
 CREATE TABLE subscription(
 SubscriptionID INT PRIMARY KEY AUTO_INCREMENT,
 SubscriptionType VARCHAR(50) CHECK (SubscriptionType IN ('Basic','Standard','Premium')),
 MonthlyFee DECIMAL(10,2)
 );
 
+--Créeation de la table review
 CREATE TABLE review(
     ReviewID INT PRIMARY KEY AUTO_INCREMENT,
     UserID INT,
@@ -16,6 +25,7 @@ CREATE TABLE review(
     ReviewDate DATE
 );
 
+--Créeation de la table movie
 CREATE TABLE movie(
     MovieID INT PRIMARY KEY AUTO_INCREMENT,
     Title VARCHAR(255),
@@ -25,6 +35,7 @@ CREATE TABLE movie(
     Rating VARCHAR(10)
 );
 
+--Créeation de la table utilisateur
 CREATE TABLE utilisateur(
     UserID INT PRIMARY KEY AUTO_INCREMENT,
     FirstName VARCHAR(100),
@@ -32,4 +43,13 @@ CREATE TABLE utilisateur(
     Email VARCHAR(100) UNIQUE,
     RegistrationDate DATE,
     SubscriptionID INT
+);
+
+--Créeation de la table watchhistory
+CREATE TABLE watchhistory(
+    WatchHistoryID INT PRIMARY KEY AUTO_INCREMENT,
+    UserID INT,
+    MovieID INT,
+    WatchDate DATE,
+    CompletionPercentage INT
 );
