5434a4ee7559131209445d5f726ed95a852cadf7
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2024-12-09 15:05:27 +0100

    Ajouter la constrainte foreign key pour la colonne SubscriptionID de la table utilisateur

--- films_db.sql
+++ films_db.sql
@@ -61,3 +61,7 @@ MODIFY COLUMN CompletionPercentage INT DEFAULT 0;
 --Ajouter la constrainte NULL pour la colonne ReviewText de la table review
 ALTER TABLE review
 MODIFY COLUMN ReviewText Text NULL;
+
+--Ajouter la constrainte foreign key pour la colonne SubscriptionID de la table utilisateur
+ALTER TABLE utilisateur 
+ADD CONSTRAINT fk_SubscriptionID FOREIGN KEY(SubscriptionID) references subscription(subscriptionID);
