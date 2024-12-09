fd56c2d6b00b179f777c3a85b8c0f9248c691ece
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2024-12-09 11:54:12 +0100

    Créer la Table subscription

--- films_db.sql
+++ films_db.sql
@@ -1,2 +1,7 @@
 CREATE DATABASE films_db;
 
+CREATE TABLE subscription(
+SubscriptionID INT PRIMARY KEY AUTO_INCREMENT,
+SubscriptionType VARCHAR(50) CHECK (SubscriptionType IN ('Basic','Standard','Premium')),
+MonthlyFee DECIMAL(10,2)
+);
